1945fd17fa215608be09de9cfa1b61c2ac90793b
BhavikAgrawal1751 <bhavikagrawal1751@gmail.com>
2024-08-09 18:04:12 +0530

    ..

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,20 +1,26 @@
-import landingImage from "../assets/landing.png"
-
+import landingImage from "../assets/landing.png";
+import appDownloadImage from "../assets/appDownload.png";
 const HomePage = () => {
-    return(
+    return (
         <div className="flex flex-col gap-12">
-             <div className="bg-write rounded-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-16">
+            <div className="bg-write rounded-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-16">
                 <h1 className="text-5xl font-bold tracking-tight text-orange-600">
                     Tuck into a takeway today
                 </h1>
                 <span className="text-xl">Food is just a click away!</span>
-             </div>
-             <div className="grid md:grid-cols-2 gap-5">
-                <img src={landingImage}/>
-                <div className="flex flex-col items-center justify-center gap-4">
-
+            </div>
+            <div className="grid md:grid-cols-2 gap-5">
+                <img src={landingImage} />
+                <div className="flex flex-col items-center justify-center gap-4 text-center">
+                    <span className="font-bold text-3x1 tracking-tighter">
+                        Order takeway even faster!
+                    </span>
+                    <span>
+                        Download the MernEats App for faster ordering and personalised recommendations
+                    </span>
+                    <img src={appDownloadImage} />
                 </div>
-             </div>
+            </div>
         </div>
     );
 };
